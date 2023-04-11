a545e3db8ca0de5b00b9a43f2b1eb05747f8ae95
gqgunhed <110590071+gqgunhed@users.noreply.github.com>
2023-04-11 15:26:00 +0200

    Update reset_windows_update.ps1

--- reset_windows_update.ps1
+++ reset_windows_update.ps1
@@ -7,10 +7,10 @@ net stop cryptsvc
 net stop lanmanworkstation
 
 # remove all files form windows update cache
-rd /s /q c:\Windows\SoftwareDistribution
+Remove-Item -Recurse -Force c:\Windows\SoftwareDistribution
 
 # start services
-net net start lanmanworkstation
+net start lanmanworkstation
 net start cryptsvc
 net start bits
 net start wuauserv
