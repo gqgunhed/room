0d0a0c56667e952e98c7257577429163e3c6d675
gqgunhed <110590071+gqgunhed@users.noreply.github.com>
2023-04-11 15:05:39 +0200

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,3 +1,12 @@
+# Install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Install software packages
 choco install -y firefox google-chrome-x64 notepadplusplus adobereader putty zoom skype vmware-horizon-client teamviewer.host choco-upgrade-all-at-startup
+
+# active windows update service
+Set-Service -Name wuauserv -StartupType Automatic
+
+# Install all windows updates
+wuauclt /detectnow
+wuauclt /updatenow
