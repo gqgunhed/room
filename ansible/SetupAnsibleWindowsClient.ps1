32efd3fa7adef5f47654c514d4105f69369a92f5
gqgunhed <110590071+gqgunhed@users.noreply.github.com>
2023-07-18 22:49:01 +0200

    Update SetupAnsibleWindowsClient.ps1

--- ansible/SetupAnsibleWindowsClient.ps1
+++ ansible/SetupAnsibleWindowsClient.ps1
@@ -1,6 +1,8 @@
 # Downloads script and executes it
 # based on https://docs.ansible.com/ansible/latest/os_guide/windows_setup.html
 #
+# file disappeared from Github, but a note here: https://docs.ansible.com/ansible/latest/dev_guide/testing/sanity/configure-remoting-ps1.html
+
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 Invoke-WebRequest https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -Outfile c:\Windows\Temp\ConfigureRemotingForAnsible.ps1
 c:\Windows\Temp\ConfigureRemotingForAnsible.ps1
